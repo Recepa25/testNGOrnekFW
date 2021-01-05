217741174022069682c136e83909c590d3cfc6db
recep <recep>
2021-01-05 11:59:23 -0700

    paralel test eklendi

---
+++ src/test/java/com/techproed/tests/D33_Data_Provider_Coklu.java
@@ -0,0 +1,14 @@
+package com.techproed.tests;
+
+public class D33_Data_Provider_Coklu {
+    //.https://qa-environment.crystalkeyhotels.com/ sayfasina gidelim
+    //2.Cucumber parametre ,cucumber scenario outline ve TestNg framework @Dataprovider kullanarak asagidaki gorevi tamamlayin
+    //  - Login tusuna basin
+    //  - Asagidaki 5 kullanici adi ve sifreyi deneyin ve login olmadigini test edin
+    //  - Manager – Manager
+    //  - Manager1- Manager1
+    //  - Manager2 - Manager2
+    //  - Manager3 - Manager3
+    //   - Manager4 – Manager4
+
+}
